540b5fba37a676402f0c5f236643f11c5dee38a8
Samarthbhat52 <Samarthrbhat52@gmail.com>
2024-05-10 20:49:50 +0530

    style(board): Added empty board page.

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
+import BlankSpace from "@/components/boards/blank-space";
 import { Button } from "@/components/ui/button";
 import { createClient } from "@/lib/supabase/client";
 
 const page = () => {
   const supabase = createClient();
 
-  return (
-    <div>
-      <Button onClick={() => supabase.auth.signOut()}>Sign out</Button>
-    </div>
-  );
+  return <BlankSpace />;
 };
 export default page;
